678e3f228906cfcf8453c13c8b40dd125c4a500e
Артём Швецов <shvetsovart55@gmail.com>
2024-02-23 13:10:45 +0300

    config + main basics

---
+++ config.py
@@ -0,0 +1,6 @@
+from aiogram import Bot,Dispatcher
+
+TOKEN = '6943843325:AAGW5mdzDAhLANZhJF-MitepOOxw3G1tgFE'
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
---
+++ main.py
@@ -0,0 +1,22 @@
+from config import bot
+from config import dp
+
+from asyncio import run
+
+from aiogram import *
+from aiogram.filters import Command
+
+main_router = Router()
+
+
+@main_router.message(Command("start"))
+async def start_handler(message: types.Message):
+    await bot.send_message(chat_id=message.chat.id, text="hello")
+
+
+async def main():
+    await dp.start_polling()
+
+
+if __name__ == "__main__":
+    run(main())
