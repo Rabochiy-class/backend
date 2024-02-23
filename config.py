57a7452ac1ee9e34ac7049eec8f44abc3fafd394
mylaff <43293264+mylaff@users.noreply.github.com>
2024-02-23 15:01:12 +0300

    add WebAppInfo & config

--- config.py
+++ config.py
@@ -1,6 +1,32 @@
-from aiogram import Bot,Dispatcher
+from logging import DEBUG
 
 TOKEN = '6943843325:AAGW5mdzDAhLANZhJF-MitepOOxw3G1tgFE'
-
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'INFO',
+            'formatter': 'standard',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+        },
+    },
+    'loggers': {
+        '': {  # root logger
+            'handlers': ['default'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        '__main__': {  # if __name__ == '__main__'
+            'handlers': ['default'],
+            'level': 'DEBUG',
+            'propagate': False
+        }
+    },
+}
--- main.py
+++ main.py
@@ -1,22 +1,49 @@
-from config import bot
-from config import dp
+import asyncio 
+import logging
+import logging.config
 
-from asyncio import run
-
-from aiogram import *
+from aiogram import Bot, Dispatcher, Router
 from aiogram.filters import Command
+from aiogram.types import Message, InlineKeyboardButton
+from aiogram.types.web_app_info import WebAppInfo
+from aiogram.types.menu_button_web_app import MenuButtonWebApp
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from config import TOKEN, LOGGING_CONFIG
 
 main_router = Router()
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+logger = logging.getLogger(__name__)
+web_app = WebAppInfo(url='https://meeting-webapp.netlify.app/login')
 
 
 @main_router.message(Command("start"))
-async def start_handler(message: types.Message):
-    await bot.send_message(chat_id=message.chat.id, text="hello")
+async def start_handler(message: Message):
+    keyboard_builder = InlineKeyboardBuilder()
+    keyboard_builder.row(
+        InlineKeyboardButton(
+            text='Перейти к приложению',
+            web_app=web_app
+        )
+    )
 
+    await message.answer('Привет!', reply_markup=keyboard_builder.as_markup())
+
+def setup_loggers():
+    logging.config.dictConfig(LOGGING_CONFIG)
 
 async def main():
-    await dp.start_polling()
+    setup_loggers()
+
+    logger.info('Starting with Telegram API key %s', TOKEN)
+
+    dp.include_router(main_router)
+
+    await bot.set_chat_menu_button(menu_button=MenuButtonWebApp(text='Открыть приложение', web_app=web_app))
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    run(main())
+    asyncio.run(main())
